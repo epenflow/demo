9e8c360ba759eb36a0e202dae829929a51916ade
epenflow <epenflow@gmail.com>
2024-12-27 21:30:54 +0800

    updates

--- src/app/one-element-scroll/_components/main.tsx
+++ src/app/one-element-scroll/_components/main.tsx
@@ -135,8 +135,6 @@ function MainHOC<T extends object>(
 					Flip.fit(oneElement, state, {
 						duration: 1,
 						ease: index === 0 ? "none" : "sine.inOut",
-						// scale: true,
-						// absolute: true,
 					}) as GSAPAnimation,
 					index ? "+=0.5" : 0
 				);
@@ -230,8 +228,8 @@ function MainHOC<T extends object>(
 				oneElement,
 				{
 					filter: "brightness(80%)",
-					// width: "100%",
-					// height: "100%",
+					width: "100%",
+					height: "100%",
 				},
 				{
 					filter: "brightness(100%)",
