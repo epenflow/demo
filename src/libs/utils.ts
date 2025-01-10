bdd868109297df97b54c9b5e56bb7601c2337792
epenflow <epenflow@gmail.com>
2025-01-10 17:46:40 +0800

    feat: add getPropertyValue utility function to retrieve CSS property values

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,5 +1,11 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 export function cn(...args: ClassValue[]) {
 	return twMerge(clsx(args));
+}
+
+export function getPropertyValue(property: string, element?: Element) {
+	const $element = element || document.documentElement;
+
+	return getComputedStyle($element).getPropertyValue(property);
 }
