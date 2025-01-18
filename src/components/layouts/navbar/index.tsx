7f5fca70094efc58bdab2fa58c5297cfdacda546
epenflow <epenflow@gmail.com>
2025-01-18 22:39:49 +0800

    fix issue : can't animate on hover

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,28 +1,56 @@
-/* eslint-disable react-refresh/only-export-components */
+import { useGSAP } from '@gsap/react';
 import { Link } from '@tanstack/react-router';
-import { useVirtualizer } from '@tanstack/react-virtual';
+import { useVirtualizer, type VirtualItem } from '@tanstack/react-virtual';
+import gsap from 'gsap';
 import React from 'react';
 import useTime from '~/hooks/use-time';
+import HeaderAnimator from '~/libs/modules/header-animator';
+import TextAnimator from '~/libs/modules/text-animator';
+import type { FileRouteTypes } from '~/routeTree.gen';
+import { useLoader } from '../loader';
 import './base.scss';
-import hoc, { type Props } from './hoc';
 
-const Navbar: React.FC<Props> = ({ scope, fnToggleMenu }) => {
-	const parentRef = React.useRef<HTMLDivElement>(null);
-	const date = useTime();
-	const { navigation } = resources;
+const Navbar: React.FC = () => {
+	const scope = React.useRef<HTMLElement>(null);
+	const virtualRef = React.useRef<HTMLDivElement>(null);
+	const timeline = React.useRef<GSAPTimeline | null>(null);
+	const [isTrigger, setTrigger] = React.useState<boolean>(false);
+
+	const { navigation } = React.useMemo(() => resources, []);
+	const { setLoaderDuration } = useLoader();
 
 	const virtualizer = useVirtualizer({
 		count: navigation.length,
-		getScrollElement: () => parentRef.current,
+		getScrollElement: () => virtualRef.current,
 		estimateSize: () => 35,
 		debug: true,
 	});
-
 	const CSSVariables = {
 		'--virtual-contentHeight': `${virtualizer.getTotalSize()}px`,
 	} as React.CSSProperties;
 
-	console.log('virtual items', virtualizer.getVirtualItems());
+	useGSAP(
+		() => {
+			const header = new HeaderAnimator(scope.current);
+			if (typeof header.timeline !== 'undefined') {
+				timeline.current = header.timeline;
+			}
+		},
+		{ scope },
+	);
+
+	React.useEffect(() => {
+		if (isTrigger) {
+			timeline.current?.play();
+		} else {
+			timeline.current?.reverse();
+			setLoaderDuration(timeline.current?.duration() || 0);
+		}
+	}, [isTrigger, setLoaderDuration]);
+
+	function fnToggle() {
+		setTrigger((prev) => !prev);
+	}
 
 	return (
 		<header
@@ -30,84 +58,93 @@ const Navbar: React.FC<Props> = ({ scope, fnToggleMenu }) => {
 			style={CSSVariables}
 			className="header--outer">
 			<div className="header--inner">
-				<button onClick={fnToggleMenu} />
+				<button onClick={fnToggle} />
 			</div>
 			<nav
 				data-lenis-prevent
 				className="header--content">
 				<div
-					ref={parentRef}
+					ref={virtualRef}
 					className="navigation--content">
 					<div className="virtual--container">
-						{virtualizer.getVirtualItems().map((virtual) => {
-							const CSSVariables = {
-								'--virtual-height': `${virtual.size}px`,
-								'--virtual-yAxis': `${virtual.start}px`,
-							} as React.CSSProperties;
-							return (
-								<Link
-									onClick={fnToggleMenu}
-									className="virtual--item"
-									style={CSSVariables}
-									to={navigation[virtual.index].to}
-									key={virtual.index}>
-									<p className="paragraph--content">
-										<span className="text--content">
-											{navigation[virtual.index].title + ' ' + virtual.index}
-										</span>
-										<span className="link--separator" />
-									</p>
-								</Link>
-							);
-						})}
+						{virtualizer.getVirtualItems().map((virtual) => (
+							<VirtualLink
+								key={virtual.index}
+								virtual={virtual}
+								fnToggle={fnToggle}
+							/>
+						))}
 					</div>
-					{/* {Array.from({ length: 10 }).map((_, key) => (
-							<Link
-								onClick={fnToggleMenu}
-								key={key}
-								to={'/'}>
-								<p>
-									<span className="text--content">{key}</span>
-									<span className="link--separator" />
-								</p>
-							</Link>
-						))} */}
 				</div>
 				<span className="separator" />
-				<div className="time--content">
-					<p suppressHydrationWarning>{date.toISOString()}</p>
-				</div>
+				<TimeContent />
 			</nav>
 		</header>
 	);
 };
-
-export default hoc(Navbar);
-const generateRandomTitle = () => {
-	const adjectives = [
-		'Amazing',
-		'Incredible',
-		'Fantastic',
-		'Wonderful',
-		'Splendid',
-		'Awesome',
-		'Impressive',
-	];
-	const nouns = ['Adventure', 'Journey', 'Experience', 'Quest', 'Odyssey', 'Saga', 'Story'];
-	const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-	const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-	return `${randomAdjective} ${randomNoun}`;
-};
-function getRandomTo() {
-	const to = ['/', '/about', '/clip-path-scroll'];
-	const randomTon = to[Math.floor(Math.random() * to.length)];
-	return randomTon;
-}
-const navigation = Array.from({ length: 10000 }).map(() => ({
-	to: getRandomTo(),
-	title: generateRandomTitle(),
-}));
-
 const resources = {
-	navigation: navigation,
+	navigation: [{ title: 'Home', to: '/' }, ...getRandomNavigation(10_000)] satisfies Array<{
+		title: string;
+		to: FileRouteTypes['to'];
+	}>,
 };
+function getRandomNavigation(length: number) {
+	const title = ['home', 'about', 'clip path scroll'];
+	const to = ['/', '/about', '/clip-path-scroll'];
+
+	return Array.from({ length }).map(() => ({
+		title: title[Math.floor(Math.random() * title.length)],
+		to: to[Math.floor(Math.random() * to.length)] as FileRouteTypes['to'],
+	}));
+}
+
+const TimeContent = () => {
+	const date = useTime();
+	return (
+		<div className="time--content">
+			<p suppressHydrationWarning>{date.toISOString()}</p>
+		</div>
+	);
+};
+const VirtualLink: React.FC<{
+	virtual: VirtualItem;
+	fnToggle: () => void;
+}> = ({ virtual, fnToggle }) => {
+	const { navigation } = React.useMemo(() => resources, []);
+	const scope = React.useRef<HTMLAnchorElement>(null);
+	const CSSVariables = {
+		'--virtual-height': `${virtual.size}px`,
+		'--virtual-yAxis': `${virtual.start}px`,
+	} as React.CSSProperties;
+
+	React.useEffect(() => {
+		const context = gsap.context(() => {
+			if (scope.current) {
+				const text = scope.current.querySelector('.text--content') as HTMLElement;
+				const textAnimator = new TextAnimator(text);
+				scope.current.addEventListener('mouseenter', () => {
+					textAnimator.animate();
+				});
+			}
+		}, scope);
+		return () => context.revert();
+	}, []);
+
+	return (
+		<Link
+			ref={scope}
+			onClick={fnToggle}
+			to={navigation[virtual.index].to}
+			className="virtual--item"
+			style={CSSVariables}>
+			<p className="paragraph--content">
+				<span className="text--content">
+					{navigation[virtual.index].title}&nbsp;{virtual.index}
+				</span>
+				<span className="link--separator" />
+			</p>
+		</Link>
+	);
+};
+
+export default Navbar;
